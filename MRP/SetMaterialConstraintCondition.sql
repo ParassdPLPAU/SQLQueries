b039ff53c687a722a4b884493e4d0e0fa5f479ae
Paras Sood <Paras.Sood@preformed.com>
2025-08-28 16:49:08 +1000

    New query to check parts with LT <=14 to non-constrained material as well

--- MRP/SetMaterialConstraintCondition.sql
+++ MRP/SetMaterialConstraintCondition.sql
@@ -137,3 +137,92 @@ UPDATE [PLP_PILOT].Erp.Part
 SET Constrained = 0 
 WHERE PartNum IN (SELECT DISTINCT * FROM Result) --order by PartNum)
 --SELECT * FROM erp.PartDtl
+
+--New query to check parts with LT <=14 to non-constrained material as well
+WITH LeadTimeCalc AS ( --Get lead times for all active parts
+	SELECT PP.PartNum, PP.Plant, PP.SourceType,
+	CASE
+		WHEN PP.SourceType = 'P' THEN PP.LeadTime
+		WHEN PP.SourceType = 'M' THEN PP.TotMfgLeadTimeSys
+		ELSE 10000
+	END AS 'LeadTime'
+	FROM PartPlant PP
+	INNER JOIN Part P ON PP.PartNum = P.PartNum
+	WHERE Plant = 'GLENDN'
+	AND P.InActive = 0
+),AllPartSOH AS (--GetAllSOH
+	SELECT PW.PartNum
+	FROM erp.PartWhse PW
+	WHERE PW.OnHandQty <> 0
+),LatestPartDemandWithinLT AS ( --Get parts with demand that has a due date within the lead time. This will not include any parts that have demand outside the LT.
+	SELECT PP.PartNum, PP.LeadTime,MAX(PD.DueDate) AS 'LatestDueDateLT',GetDATE() + LeadTime AS 'dateLT'
+	FROM LeadTimeCalc PP
+	LEFT JOIN erp.partdtl PD ON PD.PartNum = PP. PartNum
+	INNER JOIN erp.PartWhse PW ON PW.PartNum = PP.PartNum
+	WHERE PP.Plant = 'GLENDN'
+	AND PW.WarehouseCode = '190'
+	AND (GetDATE() + LeadTime >= PD.DueDate OR PD.DueDate IS NULL)
+	GROUP BY PP.PartNum,PP.LeadTime
+),TotalDemandWithinLT AS ( --Get total demand of parts within lead time that satisfy the criteria for LatestPartDemandWithinLT. This will not include any parts that have demand outside the LT.
+	SELECT PP.PartNum, 
+	SUM(PD.Quantity) AS 'Demand'
+	FROM erp.PartPlant PP
+	INNER JOIN LatestPartDemandWithinLT BASE ON BASE.PartNum = PP.PartNum
+	INNER JOIN erp.partdtl PD ON PD.PartNum = PP. PartNum
+	INNER JOIN erp.PartWhse PW ON PW.PartNum = PP.PartNum
+	WHERE PP.Plant = 'GLENDN'
+	AND (PD.Duedate <= BASE.LatestDueDateLT)
+	AND PW.WarehouseCode = '190'
+	AND PD.RequirementFlag = 1
+	GROUP BY PP.PartNum
+),ShortLT AS (
+	SELECT * FROM LeadTimeCalc
+	WHERE LeadTime <= 14
+)--SELECT * FROM ShortLT
+,Result AS (
+SELECT 
+	PP.PartNum 
+FROM 
+	erp.PartPlant PP
+INNER JOIN erp.PartWhse PW ON PP.PartNum = PW.PartNum
+INNER JOIN TotalDemandWithinLT BASE_2 ON BASE_2.PartNum = PP. PartNum
+INNER JOIN Part P ON P.PartNum = PP.PartNum
+WHERE PP.Plant = 'GLENDN'
+AND PW.WarehouseCode = '190'
+AND ((PW.OnHandQty - BASE_2.Demand) >= PP.SafetyQty)
+AND PW.OnHandQty <> 0
+AND P.Inactive = 0
+UNION -- Now get all parts that have SOH but no demand within the LT
+SELECT 
+	PartNum 
+FROM 
+	AllPartSOH
+WHERE 
+	PartNum NOT IN (
+		SELECT 
+			PartNum 
+		FROM 
+			TotalDemandWithinLT
+		)
+UNION
+SELECT
+	PartNum
+FROM
+	ShortLT
+)
+
+
+SELECT PartNum FROM erp.Part 
+WHERE 
+PartNum NOT IN
+(SELECT DISTINCT * FROM Result) 
+AND Inactive = 0
+
+--SELECT * FROM erp.PartPlant PP
+--INNER JOIN erp.Part P ON P.PartNum = PP.PartNum AND PP.Plant = 'GLENDN'
+----LEFT JOIN erp.PartDtl PD ON PD.PartNum = PP.PartNum 
+--where inactive = 0 and constrained = 0
+----AND SourceType = 'P' 
+----AND SafetyQty <> 0
+----AND LeadTime <= 14 AND SourceType = 'P'
+----AND PP.TotMfgLeadTimeSys <= 14 and SourceType = 'M'
